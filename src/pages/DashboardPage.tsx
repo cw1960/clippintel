ba2b248cb1bbe07cd180dd4716ddda461273e62a
Christopher <chrism2homefolder@Christophers-Mac-mini.local>
2025-07-08 09:19:46 -0600

    test: add useActiveNavItem to dashboard for incremental bug isolation

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Container, Title } from "@mantine/core";
 import { IconDashboard } from "@tabler/icons-react";
+import { useActiveNavItem } from "../stores/layoutStore";
 
 export const DashboardPage: React.FC = () => {
+  const { activeItem } = useActiveNavItem();
   return (
     <Container size="xl" style={{ padding: 40 }}>
       <Title order={1} style={{ color: "white" }}>
-        Dashboard Test (Mantine + Tabler)
+        Dashboard Test (Mantine + Tabler + Nav)
       </Title>
       <IconDashboard size={48} color="#36A4A0" style={{ marginTop: 24 }} />
+      <div style={{ color: "white", marginTop: 32 }}>
+        Active Nav Item: {String(activeItem)}
+      </div>
     </Container>
   );
 };
