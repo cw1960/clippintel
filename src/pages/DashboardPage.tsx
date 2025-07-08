9fa55048ecf3573486311aaf475c56002589ff29
Christopher <chrism2homefolder@Christophers-Mac-mini.local>
2025-07-08 09:29:45 -0600

    fix: call useActiveNavItem unconditionally at top level to resolve React hook order error

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,32 +4,9 @@ import { IconDashboard } from "@tabler/icons-react";
 import { useActiveNavItem } from "../stores/layoutStore";
 
 export const DashboardPage: React.FC = () => {
-  let activeItem = "unknown";
-  let selectorError = null;
-  try {
-    const nav = useActiveNavItem();
-    activeItem =
-      nav && typeof nav.activeItem === "string" ? nav.activeItem : "undefined";
-    // eslint-disable-next-line no-console
-    console.log("DashboardPage useActiveNavItem:", nav);
-  } catch (err) {
-    selectorError = err;
-    // eslint-disable-next-line no-console
-    console.error("DashboardPage useActiveNavItem error:", err);
-  }
-
-  if (selectorError) {
-    return (
-      <Container size="xl" style={{ padding: 40 }}>
-        <Title order={1} style={{ color: "red" }}>
-          DashboardPage Selector Error
-        </Title>
-        <div style={{ color: "white", marginTop: 32 }}>
-          Error: {String(selectorError)}
-        </div>
-      </Container>
-    );
-  }
+  const nav = useActiveNavItem(); // Always call hooks at the top level
+  const activeItem =
+    nav && typeof nav.activeItem === "string" ? nav.activeItem : "dashboard";
 
   return (
     <Container size="xl" style={{ padding: 40 }}>
