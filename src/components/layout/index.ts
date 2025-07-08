31baa058d72436a27ff87cc5bb1ff62c2fc760b5
Christopher <chrism2homefolder@Christophers-Mac-mini.local>
2025-07-08 10:07:57 -0600

    Scaffold new Vite + React + TS project. Ready for Mantine, Zustand, Supabase setup.

--- src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { AppShell } from "./AppShell";
-export { Header } from "./Header";
-export { Navbar } from "./Navbar";
-export * from "../../types/layout";
-export * from "../../stores/layoutStore";
