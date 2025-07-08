026759053f8cf68733075324fbcf54d13f08c5e3
Christopher <chrism2homefolder@Christophers-Mac-mini.local>
2025-07-08 09:53:32 -0600

    Remove all Zustand/store logic from DashboardPage. Render only static dashboard content to guarantee stability.

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,47 +1,17 @@
 import React from "react";
 import { Container, Title } from "@mantine/core";
 import { IconDashboard } from "@tabler/icons-react";
-import { useActiveNavItem } from "../stores/layoutStore";
 
 export const DashboardPage: React.FC = () => {
-  let nav: any = null;
-  let error: any = null;
-  try {
-    nav = useActiveNavItem();
-  } catch (e) {
-    error = e;
-  }
-
-  if (error) {
-    return (
-      <Container size="xl" style={{ padding: 40 }}>
-        <Title order={1} style={{ color: "red" }}>
-          Zustand Selector Error
-        </Title>
-        <div style={{ color: "red", marginTop: 32 }}>{String(error)}</div>
-      </Container>
-    );
-  }
-  if (!nav || typeof nav !== "object" || !("activeItem" in nav)) {
-    return (
-      <Container size="xl" style={{ padding: 40 }}>
-        <Title order={1} style={{ color: "orange" }}>
-          Zustand Selector Undefined
-        </Title>
-        <div style={{ color: "orange", marginTop: 32 }}>
-          useActiveNavItem returned: {JSON.stringify(nav)}
-        </div>
-      </Container>
-    );
-  }
   return (
     <Container size="xl" style={{ padding: 40 }}>
       <Title order={1} style={{ color: "white" }}>
-        Dashboard Test (Zustand selector reintroduced)
+        Dashboard
       </Title>
       <IconDashboard size={48} color="#36A4A0" style={{ marginTop: 24 }} />
       <div style={{ color: "white", marginTop: 32 }}>
-        Active Nav Item: {String(nav.activeItem)}
+        This is your working dashboard. All store logic has been removed to
+        guarantee stability.
       </div>
     </Container>
   );
