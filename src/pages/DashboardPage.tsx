b49eedce9d74f3d7a63c9dd4f59f2450d9dbd612
Christopher <chrism2homefolder@Christophers-Mac-mini.local>
2025-07-08 09:11:06 -0600

    test: add Tabler icon to dashboard for incremental bug isolation

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container, Title } from "@mantine/core";
+import { IconDashboard } from "@tabler/icons-react";
 
 export const DashboardPage: React.FC = () => {
   return (
     <Container size="xl" style={{ padding: 40 }}>
       <Title order={1} style={{ color: "white" }}>
-        Dashboard Test (Mantine only)
+        Dashboard Test (Mantine + Tabler)
       </Title>
+      <IconDashboard size={48} color="#36A4A0" style={{ marginTop: 24 }} />
     </Container>
   );
 };
